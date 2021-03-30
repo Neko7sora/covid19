4c000a04ca91a2f5284b7faab7c1fe8631da885b
Neko7sora <oeyvdfqbt@relay.firefox.com>
2021-03-30 20:14:21 +0900

    Update Service Worker custom js

--- static/sw-custom.js
+++ static/sw-custom.js
@@ -1,23 +1,21 @@
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open('indexpages').then(function (cache) {
-      var rooturl = [];
-      rooturl.push(location.protocol+"//"+location.host)
-      return cache.addAll([
-        ...rooturl
-      ]);
-    })
-  );
-});
-
-caches.keys().then(function(keys) {
+caches.keys().then(function (keys) {
 
   var online = [];
   online.push(navigator.onLine)
-    var promises = [];
-    keys.forEach(function(cacheName) {
-        if (cacheName == "indexpages" && online[0] == true) {
-            promises.push(caches.delete("indexpages"));
-        }
-    });
+  var promises = [];
+  keys.forEach(function (cacheName) {
+    if (cacheName == "indexpages" && online[0] == true) {
+      promises.push(caches.delete("indexpages")).then(function () {
+        self.addEventListener('install', function (event) {
+          event.waitUntil(
+            caches.open('indexpages').then(function (cache) {
+              return cache.addAll([
+                "/"
+              ]);
+            })
+          );
+        });
+      })
+    }
+  });
 });
